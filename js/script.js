43650e2be518e189cd5b14f3147681b74ba8ab3c
Clara <mariaborim573@gmail.com>
2025-07-08 20:11:32 -0300

    improve form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+var nome = document.getElementById("nome")
+var email = document.getElementById("email")
+var telefone = document.getElementById("telefone")
+
 document.getElementById("botaoenviar").addEventListener("click", validaformulario)
 
-function validaformulario(){
-    if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value != "") {
-        alert("Pronto! Você receberá as novidades por email.")
+function validaformulario(event){
+    event.preventDefault();
+
+    if (nome.value != "" &&
+        email.value.includes("@") != "" &&
+        telefone.value != ""
+    ) {
+        alert("Pronto! Você receberá as novidades por email.");
+        nome.value = "";
+        email.value = "";
+        telefone.value = "";
     } else {
         alert("Por favor, preencha os campos de nome, email e telefone!")
     }
